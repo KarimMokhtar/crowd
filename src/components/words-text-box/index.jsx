40e7f8904af96b976df046ea1c88292aff55451e
karim mokhtar <karimmokhtar28@gmail.com>
2021-03-25 21:11:20 +0200

    initializing the components structure

---
+++ src/components/words-text-box/index.jsx
@@ -0,0 +1,10 @@
+export default function WordsTextBox() {
+    return(
+        <div>
+            [type] of these words
+            separate the words with comas
+            <textarea></textarea>
+            ////////////////////////////// total characters [num]/450
+        </div>
+    )
+}
